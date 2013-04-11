8dc08a3f8a5e310c99f58ad689f49a5a0fc52b19
Phan Văn Khanh <vankhanh.k55bk@gmail.com>
2013-04-11 08:59:44 +0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FramgiaTestApp::Application.config.secret_token = '418416905f123d61190ce36961a81e1f2e734f3166202c3e8e2a4a27478ee9a9ac7d960b2065306f3b83f27f80f8b526cb512f49d34c985c916ec08af805247a'
